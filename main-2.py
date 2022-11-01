5a84f704aac2adeb8d6a498df2d8602fc8b53d61
Joel Wilkins <108821639+joelwilkins1@users.noreply.github.com>
2022-11-01 22:32:43 +0000

    Add files via upload

---
+++ main-2.py
@@ -0,0 +1,16 @@
+print('3 times table: \n')
+num=1
+for num1 in range (3,33,3):
+  print(num, 'x 3 =', num1)
+  num=num+1
+##
+  
+print('Times table: \n')
+times=int(input('What times table would you like? '))
+num=1
+for num1 in range (times,times*10, times):
+  print(num, 'x', times, '=', num1)
+  num=num+1
+    
+  
+  
